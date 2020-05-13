fac7c452ced77120f39af76e75cb2d4007bbddc8
lipengzhou <lpzmail@163.com>
2020-05-13 11:03:06 +0800

    首页文章列表-展示文章列表数据

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -22,7 +22,11 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(article, index) in articles"
+        :key="index"
+        :title="article.title"
+      />
     </van-list>
   </div>
 </template>
@@ -41,9 +45,10 @@ export default {
   },
   data () {
     return {
-      list: [], // 数据列表
+      articles: [], // 数据列表
       loading: false, // 控制加载中的 loading 状态
-      finished: false // 控制加载结束的状态，当加载结束，不再触发加载更多
+      finished: false, // 控制加载结束的状态，当加载结束，不再触发加载更多
+      timestamp: null // 获取下一页数据的时间戳
     }
   },
   computed: {},
@@ -56,14 +61,25 @@ export default {
       // 1. 请求获取数据
       const { data } = await getArticles({
         channel_id: this.channel.id, // 频道 ID
-        timestamp: 1589337751688, // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳，timestamp 相当于页码，请求最新数据使用当前最新时间戳，下一页数据使用上一次返回的数据中的时间戳
+        timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳，timestamp 相当于页码，请求最新数据使用当前最新时间戳，下一页数据使用上一次返回的数据中的时间戳
         with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
       })
 
-      console.log(data)
       // 2. 把数据放到 list 数组中
+      const { results } = data.data
+      this.articles.push(...results)
+
       // 3. 设置本次加载状态结束，它才可以判断是否需要加载下一次，否则就会永远的停在这里
+      this.loading = false
+
       // 4. 数据全部加载完成
+      if (results.length) {
+        // 更新获取下一页数据的页码
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        // 没有数据了，把加载状态设置结束，不再触发加载更多
+        this.finished = true
+      }
     }
   }
 }
