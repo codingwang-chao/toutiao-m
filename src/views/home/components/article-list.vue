4af2802aceb3f3311adabd703845f013c528bc19
lipengzhou <lpzmail@163.com>
2020-05-13 10:46:59 +0800

    首页文章列表-获取文章列表数据

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="article-list">
+    <!--
+      List 组件通过 loading 和 finished 两个变量控制加载状态，
+当组件初始化或滚动到到底部时，会触发 load 事件并将 loading 设置成 true，此时可以发起异步操作并更新数据，数据更新完毕后，将 loading 设置成 false 即可。
+若数据已全部加载完毕，则直接将 finished 设置成 true 即可。
+
+    - `load 事件`：
+    + List 初始化后会触发一次 load 事件，用于加载第一屏的数据。
+    + 如果一次请求加载的数据条数较少，导致列表内容无法铺满当前屏幕，List 会继续触发 load 事件，直到内容铺满屏幕或数据全部加载完成。
+
+  - `loading 属性`：控制加载中的 loading 状态
+    + 非加载中，loading 为 false，此时会根据列表滚动位置判断是否触发 load 事件（列表内容不足一屏幕时，会直接触发）
+    + 加载中，loading 为 true，表示正在发送异步请求，此时不会触发 load 事件
+  - `finished 属性`：控制加载结束的状态
+    + 在每次请求完毕后，需要手动将 loading 设置为 false，表示本次加载结束
+    + 所有数据加载结束，finished 为 true，此时不会触发 load 事件
+     -->
     <van-list
       v-model="loading"
       :finished="finished"
@@ -12,6 +28,8 @@
 </template>
 
 <script>
+import { getArticles } from '@/api/article'
+
 export default {
   name: 'ArticleList',
   components: {},
@@ -23,9 +41,9 @@ export default {
   },
   data () {
     return {
-      list: [],
-      loading: false,
-      finished: false
+      list: [], // 数据列表
+      loading: false, // 控制加载中的 loading 状态
+      finished: false // 控制加载结束的状态，当加载结束，不再触发加载更多
     }
   },
   computed: {},
@@ -33,22 +51,19 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
+    async onLoad () {
+      console.log('onLoad')
+      // 1. 请求获取数据
+      const { data } = await getArticles({
+        channel_id: this.channel.id, // 频道 ID
+        timestamp: 1589337751688, // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳，timestamp 相当于页码，请求最新数据使用当前最新时间戳，下一页数据使用上一次返回的数据中的时间戳
+        with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+      })
 
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 1000)
+      console.log(data)
+      // 2. 把数据放到 list 数组中
+      // 3. 设置本次加载状态结束，它才可以判断是否需要加载下一次，否则就会永远的停在这里
+      // 4. 数据全部加载完成
     }
   }
 }
