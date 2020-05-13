cb42179af8526bd8a29fd0ea0bb45603264f2bdb
lipengzhou <lpzmail@163.com>
2020-05-13 14:32:56 +0800

    首页文章列表-列表项-内容数据绑定

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="article-item">文章列表项</div>
+  <!-- <div class="article-item">文章列表项</div> -->
+  <van-cell class="article-item">
+    <div slot="title">{{ article.title }}</div>
+    <div slot="label">
+      <div
+        v-if="article.cover.type === 3"
+        class="cover-wrap"
+      >
+        <div class="cover-wrap-item">
+          <van-image
+            width="116"
+            height="73"
+            fit="cover"
+            v-for="(img, index) in article.cover.images"
+            :key="index"
+            :src="img"
+          />
+        </div>
+      </div>
+      <div class="label-wrap">
+        <span>{{ article.aut_name }}</span>
+        <span>{{ article.comm_count }}评论</span>
+        <span>{{ article.pubdate }}</span>
+      </div>
+    </div>
+    <!-- 默认插槽的名字叫 default，可以省略 -->
+    <van-image
+      v-if="article.cover.type === 1"
+      width="116"
+      height="73"
+      fit="cover"
+      :src="article.cover.images[0]"
+    />
+  </van-cell>
 </template>
 
 <script>
