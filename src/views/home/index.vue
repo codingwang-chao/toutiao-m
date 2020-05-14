0937186b0bfff035e976ca7d5d2d624e194244a6
lipengzhou <lpzmail@163.com>
2020-05-14 16:06:25 +0800

    频道编辑-正确的获取首页频道列表数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -68,6 +68,8 @@
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
 import ChannelEdit from './components/channel-edit'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 
 export default {
   name: 'HomeIndex',
@@ -83,7 +85,9 @@ export default {
       isChannelEditShow: true // 控制编辑频道的显示状态
     }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
   created () {
     this.loadChannels()
@@ -91,9 +95,30 @@ export default {
   mounted () {},
   methods: {
     async loadChannels () {
+      let channels = []
+      if (this.user) {
+        // 已登录，请求获取线上的用户频道列表数据
+        const { data } = await getUserChannels()
+        channels = data.data.channels
+      } else {
+        // 没有登录，判断是否有本地存储的频道列表数据
+        const localChannels = getItem('user-channels')
+
+        // 如果有本地存储的频道列表则使用
+        if (localChannels) {
+          channels = localChannels
+        } else {
+          // 用户没有登录，也没有本地存储的频道，那就请求获取默认推荐的频道列表
+          const { data } = await getUserChannels()
+          channels = data.data.channels
+        }
+      }
+
+      // 把处理好的数据放到 data 中以供模板使用
+      this.channels = channels
       // 请求获取频道数据
-      const { data } = await getUserChannels()
-      this.channels = data.data.channels
+      // const { data } = await getUserChannels()
+      // this.channels = data.data.channels
     }
 
     // onUpdateActive (index) {
