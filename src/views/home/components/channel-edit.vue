16c87e5d00caa80b9a41cacefef9ab85ebdb896b
lipengzhou <lpzmail@163.com>
2020-05-14 11:12:12 +0800

    频道编辑-展示频道推荐-处理完成

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -32,9 +32,9 @@
     <van-grid :gutter="10">
       <van-grid-item
         class="grid-item"
-        v-for="value in 80"
-        :key="value"
-        text="文字"
+        v-for="(channel, index) in recommendChannels"
+        :key="index"
+        :text="channel.name"
       />
     </van-grid>
   </div>
@@ -57,7 +57,41 @@ export default {
       allChannels: [] // 所有频道数据列表
     }
   },
-  computed: {},
+  computed: {
+    // 推荐的频道列表
+    recommendChannels () {
+      // 思路：所有频道 - 我的频道 = 剩下的推荐频道
+      // filter 方法：过滤数据，根据方法返回的布尔值 true 来收集数据
+      // filter 方法查找满足条件的所有元素
+      return this.allChannels.filter(channel => {
+        // 判断 channel 是否属于用户频道
+        // find 方法查找满足条件的单个元素
+        return !this.userChannels.find(userChannel => {
+          // 找到满足该条件的元素
+          return userChannel.id === channel.id
+        })
+      })
+
+      // const arr = []
+
+      // // 遍历所有频道
+      // this.allChannels.forEach(channel => {
+      //   let flag = false
+      //   for (let i = 0; i < this.userChannels.length; i++) {
+      //     if (this.userChannels[i].id === channel.id) {
+      //       // 所有频道中的频道项属于用户频道
+      //       flag = true
+      //       break
+      //     }
+      //   }
+      //   if (!flag) {
+      //     arr.push(channel)
+      //   }
+      // })
+
+      // return arr
+    }
+  },
   watch: {},
   created () {
     this.loadAllChannels()
