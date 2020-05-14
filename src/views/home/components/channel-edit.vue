5a6d30392ea90cff061546c98e8a76748c76f0c6
lipengzhou <lpzmail@163.com>
2020-05-14 10:40:24 +0800

    频道编辑-页面布局-样式

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="channel-edit">
     <van-cell center :border="false">
-      <div slot="title">我的频道</div>
+      <div
+        slot="title"
+        class="channel-title"
+      >我的频道</div>
       <van-button
         type="danger"
         plain
@@ -12,6 +15,7 @@
 
     <van-grid :gutter="10">
       <van-grid-item
+        class="grid-item"
         v-for="value in 8"
         :key="value"
         text="文字"
@@ -19,12 +23,16 @@
     </van-grid>
 
     <van-cell center :border="false">
-      <div slot="title">频道推荐</div>
+      <div
+        slot="title"
+        class="channel-title"
+      >频道推荐</div>
     </van-cell>
 
     <van-grid :gutter="10">
       <van-grid-item
-        v-for="value in 8"
+        class="grid-item"
+        v-for="value in 80"
         :key="value"
         text="文字"
       />
@@ -51,5 +59,22 @@ export default {
 <style scoped lang="less">
 .channel-edit {
   padding-top: 54px;
+
+  .channel-title {
+    font-size: 16px;
+    color: #333333;
+  }
+
+  .grid-item {
+    width: 80px;
+    height: 43px;
+    /deep/ .van-grid-item__content {
+      background-color: #f4f5f6;
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #222;
+      }
+    }
+  }
 }
 </style>
