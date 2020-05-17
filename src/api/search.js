766b3997626cf64521ac6208d9b75166989d51cb
lipengzhou <lpzmail@163.com>
2020-05-17 11:28:35 +0800

    文章搜索-联想建议-请求展示数据

---
+++ src/api/search.js
@@ -0,0 +1,17 @@
+/**
+ * 搜索相关请求模块
+ */
+import request from '@/utils/request'
+
+/**
+ * 获取搜索联想建议
+ */
+export const getSearchSuggestions = q => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/suggestion',
+    params: {
+      q // 联想建议关键词
+    }
+  })
+}
--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -2,16 +2,16 @@
   <div class="search-suggestion">
     <van-cell
       icon="search"
-      title="hello"
-    ></van-cell>
-    <van-cell
-      icon="search"
-      title="hello 123"
+      :title="str"
+      v-for="(str, index) in suggestions"
+      :key="index"
     ></van-cell>
   </div>
 </template>
 
 <script>
+import { getSearchSuggestions } from '@/api/search'
+
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -22,7 +22,9 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      suggestions: [] // 联想建议数据列表
+    }
   },
   computed: {},
   // watch 详细的使用语法参考：https://cn.vuejs.org/v2/api/#watch
@@ -35,10 +37,12 @@ export default {
     // 这才是监视的完整写法
     searchText: {
       // 当数据发生变化则会执行 handler 处理函数
-      handler () {
+      async handler () {
         // 找到数据接口
         // 请求获取数据
         // 模板绑定展示
+        const { data } = await getSearchSuggestions(this.searchText)
+        this.suggestions = data.data.options
       },
       immediate: true // 该回调将会在侦听开始之后被立即调用
     }
