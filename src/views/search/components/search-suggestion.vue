dd1ae645012a0bf05d1200a72250d9d3330ec6b0
lipengzhou <lpzmail@163.com>
2020-05-17 14:40:44 +0800

    文章搜索-搜索关键词高亮-处理完成

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -2,15 +2,17 @@
   <div class="search-suggestion">
     <van-cell
       icon="search"
-      :title="str"
       v-for="(str, index) in suggestions"
       :key="index"
-    ></van-cell>
+    >
+      <div slot="title" v-html="highlight(str)"></div>
+    </van-cell>
   </div>
 </template>
 
 <script>
 import { getSearchSuggestions } from '@/api/search'
+import { debounce } from 'lodash'
 
 export default {
   name: 'SearchSuggestion',
@@ -23,7 +25,8 @@ export default {
   },
   data () {
     return {
-      suggestions: [] // 联想建议数据列表
+      suggestions: [], // 联想建议数据列表
+      htmlStr: 'Hello <span style="color: red">World</span>'
     }
   },
   computed: {},
@@ -37,19 +40,34 @@ export default {
     // 这才是监视的完整写法
     searchText: {
       // 当数据发生变化则会执行 handler 处理函数
-      async handler () {
-        // 找到数据接口
-        // 请求获取数据
-        // 模板绑定展示
+      // debounce 函数
+      //  参数1：函数
+      //  参数2：时间
+      //  返回值：防抖处理的函数
+      handler: debounce(async function () {
         const { data } = await getSearchSuggestions(this.searchText)
         this.suggestions = data.data.options
-      },
+      }, 200),
       immediate: true // 该回调将会在侦听开始之后被立即调用
     }
   },
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    highlight (str) {
+      // a /a/gi、vue /vue/gi
+      // 正则表达式 /中间的内容/ 都会当作正则匹配模式字符来对待
+      // 错误的写法： /this.searchText/gi
+      // RegExp 是正则表达式的构造函数
+      //  参数1：字符串
+      //  参数2：匹配模式
+      //  返回值：正则对象
+      return str.replace(
+        new RegExp(this.searchText, 'gi'),
+        `<span style="color: red">${this.searchText}</span>`
+      )
+    }
+  }
 }
 </script>
 
