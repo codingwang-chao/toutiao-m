c02ba43d65007be7815c7d9137d9bc9bb92b19c2
lipengzhou <lpzmail@163.com>
2020-05-17 10:16:21 +0800

    文章搜索-页面布局-搜索栏

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="search-container">文章搜索</div>
+  <div class="search-container">
+    <!-- 搜索栏 -->
+    <!--
+      Tips: 在 van-search 外层增加 form 标签，且 action 不为空，即可在 iOS 输入法中显示搜索按钮
+     -->
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        placeholder="请输入搜索关键词"
+        show-action
+        @search="onSearch"
+        @cancel="$router.back()"
+      />
+    </form>
+    <!-- /搜索栏 -->
+
+    <!-- 联想建议 -->
+    <!-- /联想建议 -->
+
+    <!-- 历史记录 -->
+    <!-- /历史记录 -->
+
+    <!-- 搜索结果 -->
+    <!-- /搜索结果 -->
+  </div>
 </template>
 
 <script>
@@ -8,13 +32,19 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      searchText: '' // 搜索输入框的内容
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSearch () {
+      console.log('onSearch')
+    }
+  }
 }
 </script>
 
