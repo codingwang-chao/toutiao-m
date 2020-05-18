2996603fc26023af0e3498de9deef1aa03d39bcf
lipengzhou <lpzmail@163.com>
2020-05-18 15:53:05 +0800

    文章评论-文章评论列表-使用List列表组件

--- src/views/article/components/comment-list.vue
+++ src/views/article/components/comment-list.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="comment-list">评论列表</div>
+  <div class="comment-list">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item" />
+    </van-list>
+  </div>
 </template>
 
 <script>
@@ -8,13 +17,35 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
+    }
+  }
 }
 </script>
 
