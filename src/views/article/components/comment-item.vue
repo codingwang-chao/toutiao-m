d4ce9a09efe57057d33b547fe16929edac8e1e59
lipengzhou <lpzmail@163.com>
2020-05-19 10:12:31 +0800

    文章评论-评论项-封装组件

---
+++ src/views/article/components/comment-item.vue
@@ -0,0 +1,26 @@
+<template>
+  <van-cell class="comment-item" title="123"></van-cell>
+</template>
+
+<script>
+export default {
+  name: 'CommentItem',
+  components: {},
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
--- src/views/article/components/comment-list.vue
+++ src/views/article/components/comment-list.vue
@@ -7,21 +7,29 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell
+      <comment-item
+        v-for="(comment, index) in list"
+        :key="index"
+        :comment="comment"
+      />
+      <!-- <van-cell
         v-for="(comment, index) in list"
         :key="index"
         :title="comment.content"
-      />
+      /> -->
     </van-list>
   </div>
 </template>
 
 <script>
 import { getComments } from '@/api/comment'
+import CommentItem from './comment-item'
 
 export default {
   name: 'CommentList',
-  components: {},
+  components: {
+    CommentItem
+  },
   props: {
     source: {
       type: [Number, String, Object],
