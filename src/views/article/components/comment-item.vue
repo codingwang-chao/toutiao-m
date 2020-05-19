b051fd706dcfdc78cd8e0e5c9c436a07c884a126
lipengzhou <lpzmail@163.com>
2020-05-19 10:19:33 +0800

    文章评论-评论项-模板结构

--- src/views/article/components/comment-item.vue
+++ src/views/article/components/comment-item.vue
@@ -1,5 +1,31 @@
 <template>
-  <van-cell class="comment-item" title="123"></van-cell>
+  <van-cell
+    class="comment-item"
+  >
+    <van-image
+      slot="icon"
+      width="36"
+      height="36"
+      round
+      fit="cover"
+      :src="comment.aut_photo"
+    />
+    <div slot="title">
+      <div>{{ comment.aut_name }}</div>
+      <div>{{ comment.content }}</div>
+      <div>
+        <span>{{ comment.pubdate }}</span>
+        <van-button
+          round
+          size="mini"
+        >回复</van-button>
+      </div>
+    </div>
+    <div>
+      <van-icon name="good-job-o" />
+      <span>12</span>
+    </div>
+  </van-cell>
 </template>
 
 <script>
