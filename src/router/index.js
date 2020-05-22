80fa70a5e59421a580212a42c0acf0e1a54d334c
lipengzhou <lpzmail@163.com>
2020-05-22 17:03:37 +0800

    功能优化-统一处理页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { Dialog } from 'vant'
+import store from '@/store/'
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,8 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/login/')
+    component: () => import('@/views/login/'),
+    meta: { requiresAuth: false } // 添加额外的自定义数据
   },
   { // 一级路由渲染到根组件中的 router-view
     path: '/',
@@ -17,29 +20,34 @@ const routes = [
       {
         path: '', // 默认子路由
         name: 'home',
-        component: () => import('@/views/home/')
+        component: () => import('@/views/home/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/qa',
         name: 'qa',
-        component: () => import('@/views/qa/')
+        component: () => import('@/views/qa/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/video',
         name: 'video',
-        component: () => import('@/views/video/')
+        component: () => import('@/views/video/'),
+        meta: { requiresAuth: false }
       },
       {
         path: '/my',
         name: 'my',
-        component: () => import('@/views/my/')
+        component: () => import('@/views/my/'),
+        meta: { requiresAuth: false }
       }
     ]
   },
   {
     path: '/search',
     name: 'search',
-    component: () => import('@/views/search/')
+    component: () => import('@/views/search/'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/article/:articleId',
@@ -47,22 +55,57 @@ const routes = [
     component: () => import('@/views/article/'),
     // 将动态路由参数映射到组件的 props 中，无论是访问还是维护性都很方便
     // 参考文档：https://router.vuejs.org/zh/guide/essentials/passing-props.html
-    props: true
+    props: true,
+    meta: { requiresAuth: false }
   },
   {
     path: '/user/profile',
     name: 'user-profile',
-    component: () => import('@/views/user-profile')
+    component: () => import('@/views/user-profile'),
+    meta: { requiresAuth: false }
   },
   {
     path: '/user/chat',
     name: 'user-chat',
-    component: () => import('@/views/user-chat')
+    component: () => import('@/views/user-chat'),
+    meta: { requiresAuth: true }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// to: 要访问的页面路由信息
+// from: 来自哪个页面的路由信息
+// next：放行的标记
+router.beforeEach((to, from, next) => {
+  // 判断页面是否需要登录才能访问
+  if (to.meta.requiresAuth) {
+    // 如果已登录，则直接通过
+    if (store.state.user) {
+      return next()
+    }
+
+    // 没有登录，提示是否登录
+    Dialog.confirm({
+      title: '访问提示',
+      message: '该功能需要登录才能访问，确认登录吗'
+    }).then(() => { // 确认执行这里
+      router.replace({
+        name: 'login',
+        query: {
+          redirect: router.currentRoute.fullPath
+        }
+      })
+    }).catch(() => { // 取消执行这里
+      // 取消了，中断路由导航
+      next(false)
+    })
+  } else {
+    /// 不需要登录状态的页面，直接过去
+    next()
+  }
 })
 
 export default router
